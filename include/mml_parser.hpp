3bfca17928199b34cdc7e80e4d80eecd1c291660
Colin Rundel <rundel@gmail.com>
2011-08-21 01:02:40 -0400

    Added mss parsing into mml parsing

--- include/mml_parser.hpp
+++ include/mml_parser.hpp
@@ -11,9 +11,6 @@
 #include <iosfwd>
 #include <sstream>
 
-#include <parse/parse_tree.hpp>
-#include <parse/json_grammar.hpp>
-
 #include <mapnik/map.hpp>
 #include <mapnik/layer.hpp>
 #include <mapnik/config_error.hpp>
@@ -21,6 +18,12 @@
 #include <mapnik/datasource.hpp>
 #include <mapnik/datasource_cache.hpp>
 
+#include <boost/filesystem.hpp>
+#include <boost/algorithm/string.hpp>
+
+#include <mss_parser.hpp>
+#include <parse/parse_tree.hpp>
+#include <parse/json_grammar.hpp>
 #include <utility/utree.hpp>
 
 namespace carto {
@@ -43,7 +46,7 @@ struct mml_parser {
         path(path_) 
     { 
         typedef position_iterator<std::string::const_iterator> it_type;
-        tree = build_parse_tree< json_parser<it_type> >(in);    
+        tree = build_parse_tree< json_parser<it_type> >(in, path);    
     }
     
     template<class T>
@@ -72,22 +75,25 @@ struct mml_parser {
         return tree.annotations()[ut.tag()].first;
     }
     
-    void key_error(std::string const& key) {
+    void key_error(std::string const& key, utree const& node) {
         
-        std::string err = std::string("Unknown keyword: ")+key; 
+        std::stringstream err;
+        err << "Unknown variable: @" << key
+            << " at " << get_location(node).get_string() << "\n"; 
+        
         if (strict)
-            throw config_error(err);
+            throw config_error(err.str());
         else
-            std::clog << "### WARNING: " << err << std::endl;    
+            std::clog << "### WARNING: " << err.str() << std::endl;    
     }
     
     void parse_map(mapnik::Map& map)
     {
-        using spirit::utree_type;
-        typedef utree::iterator iter;
+        //using spirit::utree_type;
+        typedef utree::const_iterator iter;
         
         
-        utree root_node = tree.ast();
+        utree const& root_node = tree.ast();
         
         BOOST_ASSERT(get_node_type(root_node) == json_object);
         
@@ -98,12 +104,13 @@ struct mml_parser {
             BOOST_ASSERT((*it).size()==2);
         
             std::string key = as<std::string>((*it).front());
-            utree& value = (*it).back();  
+            utree const& value = (*it).back();  
         
             if (key == "srs") {
                 map.set_srs( as<std::string>(value) );
             } else if (key == "Stylesheet") {
-            
+                BOOST_ASSERT(get_node_type(value) == json_array);
+                parse_stylesheet(map, value);
             } else if (key == "Layer") {
                 BOOST_ASSERT(get_node_type(value) == json_array);
             
@@ -115,8 +122,55 @@ struct mml_parser {
                     parse_layer(map, (*lyr_it).front()) ;
                 }
             } else {
-                key_error(key);
+                key_error(key, *it);
             }        
+        }
+        
+        typedef std::vector<mapnik::layer> layers;
+        layers& l = map.layers(); 
+        
+        layers::iterator l_it  = l.begin(),
+                         l_end = l.end();
+        
+        for(; l_it!=l_end; ++l_it) {
+            std::string name( (*l_it).name() );
+            
+            if (name != "") {            
+                boost::algorithm::to_lower(name);
+                boost::algorithm::replace_all(name, " ", "_");
+            
+                if (map.find_style(name))
+                    (*l_it).add_style(name);
+            }
+        }
+    }
+    
+    void parse_stylesheet(mapnik::Map& map, utree const& node)
+    {
+        using namespace boost;
+        
+        typedef utree::const_iterator iter;
+        
+        iter it  = node.begin(), 
+             end = node.end();
+        
+        filesystem::path parent_dir = filesystem::path(path).parent_path();
+        
+        for (; it != end; ++it) {
+            std::string data( as<std::string>(*it) );
+            filesystem::path abs_path( data ),
+                             rel_path = parent_dir / abs_path;
+            
+            if (filesystem::exists(abs_path)) {
+                mss_parser parser = load_mss(abs_path.string(), strict);
+                parser.parse_stylesheet(map);
+            } else if (filesystem::exists(rel_path)) {
+                mss_parser parser = load_mss(rel_path.string(), strict);
+                parser.parse_stylesheet(map);
+            } else {
+                mss_parser parser(data, strict, path);
+                parser.parse_stylesheet(map);
+            }
         }
     }
     
@@ -156,7 +210,7 @@ struct mml_parser {
                 BOOST_ASSERT(get_node_type(value) == json_object);
                 parse_Datasource(lyr, value.front());
             } else {
-                key_error(key);
+                key_error(key, *it);
             }
         }
         
@@ -206,8 +260,9 @@ mml_parser load_mml(std::string filename, bool strict)
     std::ifstream file(filename.c_str(), std::ios_base::in);
 
     if (!file)
-        throw std::exception();
-
+        throw config_error(std::string("Cannot open input file: ")+filename);
+        
+    
     std::string in;
     file.unsetf(std::ios::skipws);
     copy(std::istream_iterator<char>(file),
