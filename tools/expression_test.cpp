9c73fa0a07dd112009dc10e0a2bc16a3585b1a6e
Colin Rundel <rundel@gmail.com>
2012-02-03 14:38:22 -0500

    Simple tool for playing with the expression parser

---
+++ tools/expression_test.cpp
@@ -0,0 +1,59 @@
+//#define BOOST_SPIRIT_DEBUG
+
+#include <boost/config/warning_disable.hpp>
+#include <boost/spirit/include/support_utree.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/spirit/include/phoenix_function.hpp>
+
+//#include <generate/generate_filter.hpp>
+#include <parse/carto_grammar.hpp>
+#include <parse/filter_grammar.hpp>
+#include <parse/expression_grammar.hpp>
+#include <parse/parse_tree.hpp>
+#include <utility/environment.hpp>
+#include <expression_eval.hpp>
+
+#include <position_iterator.hpp>
+
+#include <iostream>
+#include <string>
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+//  Main program
+///////////////////////////////////////////////////////////////////////////////
+int main()
+{
+
+    using boost::spirit::ascii::space;
+    using boost::spirit::utree;
+    
+    std::cout << "/////////////////////////////////////////////////////////\n\n";
+    std::cout << "Expression parser...\n\n";
+    std::cout << "/////////////////////////////////////////////////////////\n\n";
+    std::cout << "Type an expression...or [q or Q] to quit\n\n";
+
+    std::string str;
+    while (std::getline(std::cin, str))
+    {
+        if (str.empty() || str[0] == 'q' || str[0] == 'Q')
+            break;
+        
+        typedef carto::position_iterator<std::string::const_iterator> iter;
+        carto::parse_tree tree = carto::build_parse_tree< carto::expression_parser<iter> >(str);
+        
+        utree ut = tree.ast();
+        
+        std::cout << "AST: "<< ut << "\n";
+        
+        carto::expression exp(tree.ast(), tree.annotations(), carto::style_env());
+        std::cout << "Result: " << exp.eval() << "\n\n";
+    }
+
+    std::cout << "Bye... :-) \n\n";
+    return 0;
+}
+
+
